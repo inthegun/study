1deac780dbecc79f6908c5523244fbe08f39f4ba
inthegun <semin129@gmail.com>
2023-04-05 23:22:10 +0900

    스택, 큐 ,재귀함수 , DFS

---
+++ this-coding-test/python/chap5/5-3.py
@@ -0,0 +1,11 @@
+# 재귀함수 
+def recursive_function():
+    print("재귀 함수를 호출합니다.")
+    recursive_function()
+
+recursive_function()
+
+'''
+RecursionError: maximum recursion depth exceeded while calling a Python object
+재귀의 최대 깊이를 초과했다는 내용
+'''
